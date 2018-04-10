c87400aea7a6305665e20934a101a7ae3d74c246
crystalDf <chendong333@gmail.com>
2018-04-11 00:14:51 +0800

    TodoMVP

--- app/src/main/java/com/star/todomvp/util/EspressoIdlingResource.java
+++ app/src/main/java/com/star/todomvp/util/EspressoIdlingResource.java
@@ -1,5 +1,7 @@
 package com.star.todomvp.util;
 
+import android.support.test.espresso.IdlingResource;
+
 public class EspressoIdlingResource {
 
     private static final String RESOURCE = "GLOBAL";
@@ -7,5 +9,17 @@ public class EspressoIdlingResource {
     private static SimpleCountingIdlingResource sSimpleCountingIdlingResource =
             new SimpleCountingIdlingResource(RESOURCE);
 
+    public static IdlingResource getIdlingResource() {
+        return sSimpleCountingIdlingResource;
+    }
 
+    public static void increment() {
+
+        sSimpleCountingIdlingResource.increment();
+    }
+
+    public static void decrement() {
+
+        sSimpleCountingIdlingResource.decrement();
+    }
 }
--- app/src/main/java/com/star/todomvp/util/SimpleCountingIdlingResource.java
+++ app/src/main/java/com/star/todomvp/util/SimpleCountingIdlingResource.java
@@ -2,26 +2,53 @@ package com.star.todomvp.util;
 
 import android.support.test.espresso.IdlingResource;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class SimpleCountingIdlingResource implements IdlingResource {
 
     private final String mResourceName;
+    private final AtomicInteger mAtomicInteger = new AtomicInteger(0);
+    private volatile ResourceCallback mResourceCallback;
 
     public SimpleCountingIdlingResource(String resourceName) {
-        mResourceName = resourceName;
+        mResourceName = checkNotNull(resourceName);
     }
 
     @Override
     public String getName() {
-        return null;
+        return mResourceName;
     }
 
     @Override
     public boolean isIdleNow() {
-        return false;
+        return mAtomicInteger.get() == 0;
     }
 
     @Override
     public void registerIdleTransitionCallback(ResourceCallback callback) {
+        this.mResourceCallback = callback;
+    }
 
+    public void increment() {
+        mAtomicInteger.getAndIncrement();
+    }
+
+    public void decrement() {
+
+        int countVal = mAtomicInteger.decrementAndGet();
+
+        if (countVal == 0) {
+
+            if (null != mResourceCallback) {
+                mResourceCallback.onTransitionToIdle();
+            }
+        }
+
+        if (countVal < 0) {
+
+            throw new IllegalArgumentException("Counter has been corrupted!");
+        }
     }
 }
